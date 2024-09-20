068f975bfeea160de52a1d47aec6d66270c8dbef
LiCaoZ <admin@licaoz.com>
2024-09-21 00:58:04 +0800

    Refactor Footer component to use anchor tags instead of Link components

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const Footer = () => {
     return (
         <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-            <p className='text-md'><Link href="/">[家]</Link> <Link href="/full-course-schedule">[完整的课程表]</Link></p>
+            <p className='text-md'><a href="/">[家]</a> <a href="/full-course-schedule">[完整的课程表]</a></p>
             <p className='text-md desc'>Made with LOVE by <b><a href='https://caozhi.li'>Caozhi Li</a></b>, from CKG, CHN.</p>
             <p className='text-sm desc'>※ 若你的时区并非中国标准时间 [(UTC+08:00) 北京，重庆，香港，乌鲁木齐] 或其他东八区时间，课程显示可能有误。</p>
         </footer>
