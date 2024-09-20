e4dc97691a832c49a21d7256cd27f3a339b13e05
LiCaoZ <admin@licaoz.com>
2024-09-21 00:27:31 +0800

    Refactor page.tsx to include Footer component

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+            <p className='text-md desc'>Made with LOVE by <b><a href='https://caozhi.li'>Caozhi Li</a></b>, from CKG, CHN.</p>
+            <p className='text-sm desc'>※ 若你的时区并非中国标准时间 [(UTC+08:00) 北京，重庆，香港，乌鲁木齐] 或其他东八区时间，课程显示可能有误。</p>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from 'react';
 import CourseScheduleParser from './components/CourseScheduleParser';
+import Footer from './components/Footer';
 
 const App: React.FC = () => {
   interface Course {
@@ -61,10 +62,7 @@ const App: React.FC = () => {
           <CourseScheduleParser onParse={handleParsedInfo} />
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <p className='text-md desc'>Made with LOVE by <b><a href='https://caozhi.li'>Caozhi Li</a></b>, from CKG, CHN.</p>
-        <p className='text-sm desc'>※ 若你的时区并非中国标准时间 [(UTC+08:00) 北京，重庆，香港，乌鲁木齐] 或其他东八区时间，课程显示可能有误。</p>
-      </footer>
+      <Footer />
     </div>
   );
 };
